fda35fcca6500d775916ed8ec94e2fb639fdf291
github username <github email address>
2019-07-22 11:10:05 -0700

    Lab complete

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,42 @@
 class Instructor
+    attr_reader
+    attr_accessor :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def pass_student(student, testname)
+        test_to_pass = BoatingTest.all.find do |test|
+            test.testname == testname && test.student == student
+        end
+            if test_to_pass
+                test_to_pass.teststatus = "passed"
+            else 
+            BoatingTest.new(student, testname, "passed", self)
+            end
+
+            test_to_pass
+    end
+
+
+    def fail_student(student, testname)
+        test_to_pass = BoatingTest.all.find do |test|
+            test.testname == testname && test.student == student
+        end
+            if test_to_pass
+                test_to_pass.teststatus = "failed"
+            else 
+            BoatingTest.new(student, testname, "failed", self)
+            end
+
+            test_to_pass
+    end
 end
+
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,45 @@
 class Student
+    attr_reader
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(testname, teststatus, instructor)
+        BoatingTest.new(self, testname, teststatus, instructor)
+    end
+
+    def find_student(name)
+        self.all.find do |student|
+            student.name == name
+        end
+    end
+
+    def grade_percentage
+        passed_tests = self.tests.select do |test|
+            test.teststatus == "passed"
+        end
+        percentage = passed_tests.length.to_f / self.tests.length.to_f
+        percentage.to_f
+
+
+    end
+
+    def tests
+        BoatingTest.all.select do |test|
+            test.student == self
+            
+        end
+        
+    end
+
+
 
 end
